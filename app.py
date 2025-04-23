0f203295db15f27978d78febf5794995252fc883
kouki004 <kawtherbouanani@gmail.com>
2025-04-23 15:28:19 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+ 
+ 
+import pandas as pd
+ 
+ 
+st.title("Mini-app Streamlit")
+ 
+ 
+df=pd.DataFrame(f"A": [1,2,3], "B": [4,5,6])
+ 
+ 
+st.write("Exemple de DataFrame :", df)
