5341f433153095ef9cc4e41cd97ebfeac73f3ced
kouki004 <kawtherbouanani@gmail.com>
2025-04-23 15:49:09 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
 import pandas as pd
 st.title("Mini-app Streamlit")
-df=pd.DataFrame(f"A": [1,2,3], "B": [4,5,6])
+df=pd.DataFrame({"A":[1,2,3], "B":[4,5,6]})
 st.write("Exemple de DataFrame :", df)
